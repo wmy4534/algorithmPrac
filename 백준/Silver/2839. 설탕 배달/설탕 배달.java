2c7abfe289287c7005446bd3e015e11974510ff7
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-01-29 13:54:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int weight = sc.nextInt();
-
-        int q = weight/5;
-        int r = weight%5;
-        int num = q;
-        for (int i=0; i<=q; i++) {
-            if (r % 3 != 0) {
-                num--;
-                r += 5;
-            }
-            else {
-                num += r/3;
-                break;
-            }
-        }
-        if (num==0) {
-            num = -1;
-        }
-        System.out.println(num);
-
-
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n= Integer.parseInt(br.readLine());
+		int result=0;
+		
+		while(n>0) {
+			if(n%5==0) { // 5의 배수이면
+				result += n/5;
+				System.out.println(result);
+				return;
+			}
+			if(n<3) {
+				System.out.println(-1);
+				return;
+			} // 3을 빼면 5로 나뉘어지는지 계속 돌면서 체크
+			n-=3;
+			result++;
+		}
+		
+		System.out.println(result);
+	}
 }
