41362483f02b74dd01be1431a9995e22b41e1dc4
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-01-31 21:46:45 +0900

    [Bronze II] Title: 방 배정, Time: 116 ms, Memory: 13972 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int studentNum = Integer.parseInt(st.nextToken()); // 학생 수
+        int roomCapacity = Integer.parseInt(st.nextToken()); // 방 최대 수용 인원
+        int[][] students = new int[2][7];
+        for (int n = 0; n < studentNum; n++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int grade = Integer.parseInt(st.nextToken());
+            students[s][grade]++;
+        }
+        int roomCount = 0;
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 1; j < 7; j++) {
+                if (students[i][j] > 0) {
+                    roomCount++;
+                }
+                if (students[i][j] / (double)roomCapacity > 1.0) {
+                    roomCount += students[i][j]/roomCapacity;
+                }
+            }
+        }
+        System.out.println(roomCount);
+    }
+}
