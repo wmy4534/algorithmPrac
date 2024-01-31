c8a86ad420e1d06f0a39b78a33a734ec57bd6eb2
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-01-31 17:28:55 +0900

    [Silver III] Title: 수열, Time: 272 ms, Memory: 24140 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -10,20 +10,25 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[] temp = new int[N];
+		int[] prefixSum = new int[N];
 		st = new StringTokenizer(br.readLine());
 		for (int d = 0; d < N; d++) {
 			temp[d] = Integer.parseInt(st.nextToken());
 		}
-		int tempSum = 0;
-		int maxTempSum = Integer.MIN_VALUE;
-		for (int i = 0; i < N-K+1; i++) {
-			tempSum = 0;
-			for(int j = i; j < i+K; j++) {
-				tempSum += temp[j];
-			}
-			if (maxTempSum < tempSum)
-				maxTempSum = tempSum;
+		prefixSum[0] = temp[0];
+		for (int i = 1; i < N; i++) {
+			prefixSum[i] = prefixSum[i-1]+temp[i];
 		}
-		System.out.println(maxTempSum);
+		
+		int max = prefixSum[K-1];
+		
+		for (int i = K; i < N; i++) {
+			int tmp = prefixSum[i]-prefixSum[i-K];
+			if (max < tmp)
+				max = tmp;
+		}
+		if(K == N)
+			max = prefixSum[K-1];
+		System.out.println(max);
 	}
 }
