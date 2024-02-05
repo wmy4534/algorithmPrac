78270cd3c6f54dd7e1e21fff26c4c99ddbb7555d
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-02-05 12:30:46 +0900

    [D4] Title: 쇠막대기 자르기, Time: 152 ms, Memory: 27,616 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -1,28 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int tc = Integer.parseInt(br.readLine());
-		for(int i = 1; i <= tc; i++) {
-			String input = br.readLine();
-			int stickNum = 0;
-			int sum = 0;
-			for(int j = 0; j < input.length(); j++) {
-				if (input.codePointAt(j) == '(') {
-					stickNum++;
-				} else if(input.codePointAt(j) == ')' && input.codePointAt(j-1) == '(') {
-					stickNum--;
-					sum += stickNum;
-				} else if(input.codePointAt(j) == ')') {
-					stickNum--;
-					sum++;
+		for(int t = 1; t <= tc; t++) {
+			Stack<Character> stack = new Stack<>();
+			char[] c = (br.readLine()).toCharArray();
+			int result = 0;
+			for(int i = 0; i < c.length; i++) {
+				if (c[i] == '(') {
+					stack.push(c[i]);
+				} else if (c[i-1] == '(') {
+					stack.pop();
+					result += stack.size();
+				} else {
+					stack.pop();
+					result++;
 				}
 			}
-			sb.append("#"+i+" "+sum+"\n");		
+			sb.append("#"+t+" "+result+"\n");
 		}
 		System.out.println(sb);
 	}
