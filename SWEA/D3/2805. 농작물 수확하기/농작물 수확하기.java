30478d9fa57d39967a8811ab8111d0d9cd36eea9
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-02-15 15:29:26 +0900

    [D3] Title: 농작물 수확하기, Time: 117 ms, Memory: 20,664 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,37 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int tc = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= tc; i++) {
-			int size = Integer.parseInt(br.readLine());
-			int[][] map = new int[size][size];
-			for (int r = 0; r < size; r++) {
+		for (int t = 1; t <= tc; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] map = new int[N][N];
+			for (int r = 0; r < N; r++) {
 				String str = br.readLine();
-				for (int c = 0; c < size; c++) {
-					map[r][c] = str.charAt(c)-'0';
+				for (int c = 0; c < N; c++) {
+					map[r][c] = str.charAt(c) - '0';
 				}
 			}
-			
-			int profit = 0;
-			for(int r = 0; r < size; r++) {
-				if (r <= size/2) {
-					for(int c = size/2-r; c <= size/2+r; c++) {
-						profit += map[r][c];
+			int curR = N / 2;
+			int curC = 0;
+			int sum = 0;
+			for (int i = 1; i <= N; i++) {
+				if (i % 2 == 1) {
+					for (int j = 0; j < N / 2; j++) { // 홀수번 째로 대각선으로 수확
+						sum += map[curR][curC];
+						curR++;
+						curC++;
 					}
 				} else {
-					for(int c = r-size/2; c <= size/2*3-r; c++) {
-						profit += map[r][c];
+					for (int j = 0; j < N / 2 - 1; j++) { // 짝수번째로 대각선 수확
+						sum += map[curR][curC];
+						curR--;
+						curC--;
 					}
 				}
+				sum += map[curR][curC];
+				curR--;
 			}
-			sb.append("#"+i+" "+profit+"\n");
+			sb.append("#"+t+" "+sum).append("\n");
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
