386c9060f27b32410de55b9284673eff3cae62e1
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-02-16 12:05:37 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 510 ms, Memory: 162,520 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -1,43 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int tc = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= tc; t++) {
-			int numCount = Integer.parseInt(br.readLine());
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] numArr = new int[numCount];
-			for (int i = 0; i < numCount; i++) {
-				numArr[i] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			Arrays.sort(numArr);
-			int result = -1;
-			for (int i = numCount - 1; i > 0; i--) {
-				for (int j = i - 1; j >= 0; j--) {
-					int multi = numArr[i] * numArr[j];
-					if (multi > result) {
-						String num = multi + "";
-						boolean check = true;
-						for (int k = 0; k < num.length() - 1; k++) {
-							if (num.codePointAt(k) - '0' > num.codePointAt(k + 1) - '0') {
-								check = false;
-								break;
-							}
-						}
-						if (check) {
-							result = multi;
-							break;
-						}
+
+			int max = (arr.length == 1 ? arr[0] : -1);
+			for (int i = 0; i < N - 1; i++) {
+				Loop: for (int j = i + 1; j < N; j++) {
+					int num = arr[i] * arr[j];
+					String tmp = num + "";
+					for (int c = 0; c < tmp.length()-1; c++) {
+						if (tmp.charAt(c) > tmp.charAt(c + 1))
+							continue Loop;
 					}
+					if (max < num)
+						max = num;
 				}
 			}
-			sb.append("#" + t + " " + result + "\n");
+			sb.append("#" + t + " " + max + "\n");
 		}
 		System.out.println(sb);
 	}
