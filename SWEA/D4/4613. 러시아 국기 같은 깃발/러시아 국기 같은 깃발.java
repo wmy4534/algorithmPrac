8853d445db94729e44d7394a37e516a989b82a26
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-02-16 11:09:48 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 122 ms, Memory: 19,776 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
