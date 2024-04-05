cd1dea2cad5cb569a954fcd93834415eca5aa029
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-04-06 02:04:23 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 동전 종류의 개수
-        int k = sc.nextInt(); // 목표 금액
-        int[] coins = new int[n]; // 동전의 가치를 저장할 배열
-        int[] dp = new int[k + 1]; // 경우의 수를 저장할 배열
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] coins = new int[n];
+		int[] dp = new int[k + 1];
 
-        // 동전의 가치 입력받기
-        for (int i = 0; i < n; i++) {
-            coins[i] = sc.nextInt();
-        }
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
 
-        dp[0] = 1; // 0원을 만들 수 있는 경우의 수는 1가지
-        for (int i = 0; i < n; i++) {
-            for (int j = coins[i]; j <= k; j++) {
-                dp[j] += dp[j - coins[i]];
-            }
-        }
+		dp[0] = 1; // 0원은 동전 모두 안쓰는 경우인 1
+		for (int i = 0; i < n; i++) {
+			for (int j = coins[i]; j <= k; j++) {
+				dp[j] += dp[j - coins[i]];
+			}
+		}
 
-        System.out.print(dp[k]);
-    }
+		System.out.print(dp[k]);
+	}
 }
