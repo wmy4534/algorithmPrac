f2a8a857c13ba0b5a12b1b4ec095c17e1fa70020
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-07-13 18:43:27 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 176 ms, Memory: 19468 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,136 +1,81 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.Scanner;
-
-/*
-	빈 칸: 언제나 이동할 수 있다. ('.')
-	벽: 절대 이동할 수 없다. ('#')
-	열쇠: 언제나 이동할 수 있다. 이 곳에 처음 들어가면 열쇠를 집는다. ('a', 'b', 'c', 'd', 'e', 'f')
-	문: 대응하는 열쇠가 있을 때만 이동할 수 있다. ('A', 'B', 'C', 'D', 'E', 'F')
-	민식이의 현재 위치: 빈 곳이고, 민식이가 현재 서 있는 곳이다. ('0')
-	출구: 달이 차오르기 때문에, 민식이가 가야하는 곳이다. 이 곳에 오면 미로를 탈출한다. ('1')
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class Minsik {
-//		public int cnt = -1;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		char[][] map = new char[N][M];
+		int[] start = new int[3];
 
-		public boolean[] keys = new boolean[6]; // 가지고 있는 키 ('a', 'b', 'c', 'd', 'e', 'f')
-
-		public Minsik() {
-		}
-
-//		public Minsik(int cnt) {
-//			this.cnt = cnt;
-//		}
-
-		public void getKey(char key) { // 키를 획득함
-			this.keys[key - 'a'] = true;
-		}
-
-		public boolean isSame(Minsik minsik) { // 같은 민식이인지 확인
-			for (int i = 0; i < 6; i++) {
-				if (this.keys[i] != minsik.keys[i]) // 만약 가지고 있는 키가 다르다면 다른 민식
-					return false;
-			}
-			return true; // 모든 키가 같다면 같은 민식
-		}
-	}
-
-	static int N;
-	static int M;
-	static char[][] map;
-
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static List<Minsik>[][] escapeMap;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		map = new char[N][M];
-		escapeMap = new ArrayList[N][M];
+		Set<Integer>[][] visited = new HashSet[N][M];
 		for (int r = 0; r < N; r++) {
+			String str = br.readLine();
 			for (int c = 0; c < M; c++) {
-				escapeMap[r][c] = new ArrayList<>();
-			}
-		}
-
-		int curR = 0;
-		int curC = 0;
-		int cnt = 0;
-		for (int r = 0; r < N; r++) {
-			String str = sc.next();
-			for (int c = 0; c < M; c++) {
+				visited[r][c] = new HashSet<>();
 				map[r][c] = str.charAt(c);
 				if (map[r][c] == '0') {
-					escapeMap[r][c].add(new Minsik());
-					curR = r;
-					curC = c;
+					map[r][c] = '.';
+					start[0] = r;
+					start[1] = c;
+					visited[r][c].add(0);
 				}
 			}
 		}
-//		System.out.println(escapeMap[curR][curC].get(0).cnt);
-//		Minsik tmp = new Minsik();
-//		tmp.keys = escapeMap[curR][curC].get(0).keys;
-//		System.out.println(Arrays.toString(escapeMap[curR][curC].get(0).keys));
-//		System.out.println(Arrays.toString(tmp.keys));
-//		tmp.getKey('a');
-//		System.out.println(Arrays.toString(escapeMap[curR][curC].get(0).keys));
-//		System.out.println(Arrays.toString(tmp.keys));
 
-		int result = -1;
-		Deque<int[]> dq = new ArrayDeque<>();
-		dq.offer(new int[] { curR, curC });
-		Bfs: while (!dq.isEmpty()) {
-			int size = dq.size();
-			cnt++;
-			for (int i = 0; i < size; i++) {
-				int[] cur = dq.pollFirst();
-				curR = cur[0];
-				curC = cur[1];
-				if (map[curR][curC] == '1') {
-//					System.out.println("도착" + curR + ", " + curC); // 도착 알림
-					result = cnt - 1;
-					break Bfs;
-				}
-				for (int m = 0; m < escapeMap[curR][curC].size(); m++) {
-					path: for (int idx = 0; idx < 4; idx++) {
-						Minsik minsik = new Minsik();
-						minsik.keys = escapeMap[curR][curC].get(m).keys.clone(); // 지도에서 위치한 민식이
-//						System.out.println("민식이 생성");
-						int nr = curR + dr[idx];
-						int nc = curC + dc[idx];
-						if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != '#') {
-//							System.out.println("새로운 좌표 : " + nr + ", " + nc);
-							if (map[nr][nc] - 'a' >= 0 && map[nr][nc] - 'a' < 6) {
-								minsik.getKey(map[nr][nc]);
-							} else if (map[nr][nc] - 'A' >= 0 && map[nr][nc] - 'A' < 6) {
-								if (!minsik.keys[map[nr][nc] - 'A']) {
-//									System.out.println("열쇠를 가지고 있지 않아! 돌아가!!!!");
-									continue path; // 열쇠를 가지고 있지 않으면 패스
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(start);
+		int[] dr = { -1, 1, 0, 0 };
+		int[] dc = { 0, 0, -1, 1 };
+		int answer = -1;
+		int moving = 0;
+		while (!queue.isEmpty()) {
+			moving++;
+			int size = queue.size();
+			for (int n = 0; n < size; n++) {
+				int[] tmp = queue.poll(); // r, c, keys
+				int keys = tmp[2];
+				for (int i = 0; i < 4; i++) {
+					int nr = tmp[0] + dr[i];
+					int nc = tmp[1] + dc[i];
+					if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc].contains(keys)) {
+						switch (map[nr][nc]) {
+						case '.':
+							visited[nr][nc].add(keys);
+							queue.offer(new int[] { nr, nc, keys });
+							break;
+						case '#': // wall
+							break;
+						case '1': // goal
+							System.out.println(moving);
+							return;
+						default: // key or door
+							if (map[nr][nc] >= 'a' && map[nr][nc] <= 'f') {
+								int num = map[nr][nc] - 'a';
+								int key = tmp[2] | (1 << num);
+								visited[nr][nc].add(key);
+								queue.offer(new int[] { nr, nc, key });
+							} else if (map[nr][nc] >= 'A' && map[nr][nc] <= 'F') {
+								int num = map[nr][nc] - 'A';
+								if ((keys & (1 << num)) != 0) {
+									visited[nr][nc].add(keys);
+									queue.offer(new int[] { nr, nc, keys });
 								}
 							}
-							for (int j = 0; j < escapeMap[nr][nc].size(); j++) { // 움직이는 곳에 같은 민식이가 있는지 확인
-								if (escapeMap[nr][nc].get(j).isSame(minsik)) { // 만약에 같은 민식이가 있다면 패스
-//									System.out.println("이미 중복된 민식이가 있어");
-									continue path;
-								}
-							}
-//							System.out.println("중복된 민식이가 없어");
-							escapeMap[nr][nc].add(minsik);
-//							System.out.println("민식이 더하기" + nr + ", " + nc);
-							dq.add(new int[] { nr, nc });
+							break;
 						}
 					}
 				}
 			}
-
 		}
-
-		System.out.println(result);
+		System.out.println(answer);
 	}
 }
