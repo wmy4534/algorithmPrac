78cbe6a2587da0d016cdf618a8f08cfcfc84c789
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-12-18 23:04:41 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 180 ms, Memory: 19432 KB -BaekjoonHub

--- 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.java
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.java
@@ -5,20 +5,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
     	String N = br.readLine();
-    	String str = new String();
     	int max = Integer.parseInt(N);
     	int length = 0;
     	for (int i = 1; i <= max; i++) {
     		String num = Integer.toString(i);
+    		sb.append(num);
     		length += num.length();
-    		str = str.concat(num);
-    		int idx = str.indexOf(N);
+    		int idx = sb.indexOf(N);
     		if (idx >= 0) {
-    			System.out.println(length - str.length() + idx + 1);
+    			System.out.println(length - sb.length() + idx + 1);
     			return;
     		}
-    		str = str.substring(Math.max(0, str.length() - 6));
+    		if (sb.length() > 6)
+    			sb.delete(0, sb.length() - 6);
     	}
     }
 }
