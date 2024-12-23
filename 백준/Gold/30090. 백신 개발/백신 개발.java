9f00a46b2e24376a0e5417b69737e1b3baa01b2d
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2024-12-23 23:02:05 +0900

    [Gold V] Title: 백신 개발, Time: 100 ms, Memory: 14108 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Gold/30090. 백신 개발/백신 개발.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static String[] strArr;
+	static int N;
+	static int answer = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		strArr = new String[N];
+		for (int i = 0; i < N; i++) {
+			strArr[i] = br.readLine();
+		}
+
+		for (int i = 0; i < N; i++) {
+			int visit = 1 << i;
+			String str = strArr[i];
+			backTracking(visit, str);
+		}
+		System.out.println(answer);
+	}
+
+	private static void backTracking(int visit, String str) {
+		if ((visit == (1 << N) - 1)) {
+			answer = Math.min(answer, str.length());
+			return;
+		}
+		int visited = visit;
+		int length = str.length();
+		for (int i = 0; i < length; i++) {
+			String newStr = str.substring(0, length-i);
+			for (int j = 0; j < N; j++) {
+				if ((visited & 1 << j) == 0 && strArr[j].endsWith(newStr)) {
+					visited |= (1 << j);
+					String tmp = strArr[j] + str.substring(length - i);
+					backTracking(visit | (1 << j), tmp);
+				}
+			}
+		}
+		visited = visit;
+		for (int i = 1; i < N; i++) {
+			String newStr = str.substring(i, length);
+			for (int j = 0; j < N; j++) {
+				if((visited & 1 << j) == 0 && strArr[j].startsWith(newStr)) {
+					visited |= (1 << j);
+					String tmp = str.substring(0, length - i) + strArr[j];
+					backTracking(visit | (1 << j), tmp);
+					
+				}
+			}
+		}
+	}
+}
