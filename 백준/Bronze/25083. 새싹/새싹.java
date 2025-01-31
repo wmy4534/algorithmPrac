9eb08b6f7a274790743d787200136876836d96aa
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2025-01-31 23:39:48 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String 새싹 = "         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |";
+		System.out.println(새싹);
+	}
+}
