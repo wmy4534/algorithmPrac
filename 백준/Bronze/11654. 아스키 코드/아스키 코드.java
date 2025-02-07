870cda86d7d432ac5422aa7051b73eef0a53a1ec
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2025-02-07 23:50:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int c = str.charAt(0);
+		System.out.println(c);
+	}
+}
