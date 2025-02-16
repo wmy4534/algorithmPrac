190f61ef9f23af4c8d29dd138ce73109d05012c8
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2025-02-16 22:29:44 +0900

    [Bronze V] Title: N 찍기, Time: 180 ms, Memory: 17952 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -5,13 +5,12 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			bw.write(i + "\n");
+			sb.append(i).append("\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.print(sb);
 		br.close();
 	}
 }
