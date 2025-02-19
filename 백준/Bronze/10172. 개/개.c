3efb52efc15ef2c8c76adc8578d3102a56f62450
wmy4534 <57590772+wmy4534@users.noreply.github.com>
2025-02-19 23:16:42 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char dog[] = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+    printf("%s", dog);
+    return 0;
+}
